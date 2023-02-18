afc4c422522b638911c75c0f09b4c149b337b492
Marilyn Marquez <marilynmarquez@MMarquez.local>
2023-02-17 20:54:29 -0600

    practice

---
+++ main.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+#if I need xml: import lxml (need to install module), then use 'lxml' instead of html
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser') #represents the whole html code
+
+#soup.elementIWant, for example:
+# print(soup.title) #full line where title tag is
+
+#treat it like a pyhthon object:
+# print(soup.title.name) #name of the title tag
+# print(soup.title.string) #what's between the title tags (a string)
+# print(soup.a) #first anchor tag it finds
+# print(soup.prettify()) #prints the whole html text but "pretty" (easier to read)
+
+#To find all of the elements of one tag: use find_all(). Examples:
+all_anchor_tags = soup.find_all(name='a') #finds all tags with name = 'a', returns a list
+print(all_anchor_tags)
+
+#to get a hold of the text inside the anchor tags:
+# for tag in all_anchor_tags:
+    # print(tag.getText()) #bs4 method
+
+#to get a hold of the links themselves:
+for tag in all_anchor_tags:
+    print(tag.get("href"))
