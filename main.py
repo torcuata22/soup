4a25642d3421356d0b7fba1dfaffbf5b3d030aa1
Marilyn Marquez <marilynmarquez@MMarquez.local>
2023-02-19 18:06:15 -0600

    more soup

--- main.py
+++ main.py
@@ -9,6 +9,30 @@ text = article_tag.get_text() #gets only the text from the link
 article_link=article_tag.get("href")
 article_upvotes=soup.find(name="span", class_="score").getText() #without getText() it just gets the tag
 
-print(text)
-print(article_link)
+# print(text)
+# print(article_link)
+# print(article_upvotes)
+
+#To get all results: change find for find_all():
+articles = soup.find_all(name="a", rel="nofollow")
+
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+    article_text = article_tag.getText()
+    article_texts.append(article_text)
+
+    article_link = article_tag.get("href")
+    article_links.append(article_link)
+
+
+# article_upvotes = [score.getText() for score in soup.find_all(name="span", class_="score")]
+#to get only point numbers, we can split the string by the space and grab the first item:
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+print(article_texts)
+print(article_links)
 print(article_upvotes)
+
+#Sort from highest upvotes to lowest:
